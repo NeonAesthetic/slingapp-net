d2a233a18c42eb1ce8b6f870ce9cfc3455482a34
Ian Murphy <3jackdaws@gmail.com>
2016-10-14 10:18:26 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 10/14/16
+ * Time: 8:09 AM
+ */
+
+?>
+<html>
+<head>
+    <title>
+        Sharing Site
+    </title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="custom.css">
+</head>
+<body>
+<div class="container-fluid" style="text-align: center; background-color: #4286f4; position: relative; top: 50%; height: 50%;">
+    <div class="col-lg-12">
+        <button class="btn-main" style="margin-top: 10%">
+            Share Screen
+        </button>
+    </div>
+</div>
+</body>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
