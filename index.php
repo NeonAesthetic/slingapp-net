f705a9ba4cfcd18b43c7bd1cb2b4976505b0d172
Ian Murphy <3jackdaws@gmail.com>
2016-10-15 15:28:59 -0700

    initial commit

--- index.php
+++ index.php
@@ -17,13 +17,33 @@
     <link rel="stylesheet" href="custom.css">
 </head>
 <body>
-<div class="container-fluid" style="text-align: center; background-color: #4286f4; position: relative; top: 50%; height: 50%;">
-    <div class="col-lg-12">
-        <button class="btn-main" style="margin-top: 10%">
-            Share Screen
-        </button>
+<div class="screenshot" style="width: 600px; min-height: 400px; top: 150px; text-align: center">
+
+</div>
+<h1 style="width:100%; text-align: center; position: absolute; top: 80px;">Sharing so easy you'll literally die</h1>
+<div class="container-fluid" style="text-align: center; background-color: #4286f4; position: relative; top: 80%; height: 20%">
+    <div class="container" style="margin-top: 50px;">
+
+        <div class="col-lg-6">
+            <button class="btn-main" style=" width: 200px">
+                Share Screen
+            </button>
+        </div>
+        <div class="col-lg-6">
+            <form class="" style="" >
+                <input name="room" class="mp-form" type="text" size="8" style="width: 200px; padding-left: 20px;" value="Join a Room" placeholder="Room Code" onclick="onetimechange(this)">
+            </form>
+        </div>
     </div>
 </div>
 </body>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script>
+    function onetimechange(e) {
+        e.value = "";
+        e.style.color = "black";
+        e.style.backgroundColor = "#fefefe";
+        e.onclick = null;
+    }
+</script>
 </html>
