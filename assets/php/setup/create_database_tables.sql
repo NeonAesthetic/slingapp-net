c8a7adf568ee734ba843b0358ffa4b1ac05896e2
Ian Murphy <3jackdaws@gmail.com>
2016-11-10 11:41:12 -0800

    Add collation to db create cript

--- assets/php/setup/create_database_tables.sql
+++ assets/php/setup/create_database_tables.sql
@@ -1,6 +1,9 @@
+DROP USER 'sling'@'localhost';
+DROP DATABASE if exists sling;
+
 CREATE USER 'sling'@'localhost';
 
-CREATE DATABASE sling;
+CREATE DATABASE sling CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 GRANT ALL PRIVILEGES ON sling.* TO 'sling';
 
