ec92e1b5aef3b4041351fd6b9f532bbf7e73c133
Tristan Loomis <tristanjloomis@gmail.com>
2017-03-13 19:37:35 -0700

    New Webpage look inplace.

--- index.php
+++ index.php
@@ -22,72 +22,44 @@ require_once "classes/Account.php";
     <link rel="stylesheet" href="/assets/css/sling.css">
 </head>
 <!--<body style="overflow: hidden; ">-->
-<body style="background-color: white; overflow: hidden">
-<div class="slingBackground">
-    <div class="slingContentContainer">
-<!--        <div class="slingWIPDiv">-->
-<!--            <div class="slingWIPDiv" style="background-color: white; width: 98%; height: 10%; margin-left: 1%; margin-right: 1%; margin-top: 4%;">-->
-<!--                <span style="margin-left: 10%; color:#38474F; font-family:'Century Gothic'; font-size: 1.5vw; overflow: hidden;">[ Your Recent Rooms ]</span>-->
-<!--            </div>-->
-<!--            <div class="slingContentDiv" id= "RecentRooms" style="background-color: transparent; width: 98%; height: 80%; margin-left: 1%; margin-right: 1%;">-->
-<!---->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="slingContentDiv" style="border: none; height: 76%; margin-left: 1%; margin-right: 1%; margin-top: 6%; width: 32%; min-width: 240px;    box-shadow: 10px 10px 5px #12171a;">-->
-<!--            <div class="slingContentDiv" style="background-color: #333333; width: 98%; height: 20%; margin-left: 1%; margin-right: 1%; margin-top: 10%;">-->
-<!--<!--                <img style="margin-left: 22%;" src="SlingImg.PNG" alt="Sling S" height="120" width="400">-->-->
-<!--                <img style="height: 100%; width: 100%; object-fit: contain" src="SlingLogo.png"/>-->
-<!--                <!--                <span style="margin-left: 1%; color:black; font-weight: lighter; font-family:'Century Gothic'; min-font-size: large; font-size: 550%;">L I N G</span>-->-->
-<!--            </div>-->
-<!--            <div class="slingContentDiv" style="background-color: #333333; width: 98%; height: 40%; margin-left: 1%; margin-right: 1%; margin-top: 4%; font-size: 0.9vw; overflow: hidden;">-->
-<!--                <div class="slingContentDiv" style="background-color: transparent; width: 98%; height: 85%; margin-left: 1%; margin-right: 1%;">-->
-<!--                    <p style="color:white; text-indent: 50px;"> The Sling application is a quick and easy tool for multiple screen sharing and collaboration-->
-<!--                        across different platforms. This site allows you to create, and participate in different rooms with any group of people also using the-->
-<!--                        application. To make a new room, simply click 'Create Room' and name the new space for your group to share for collaboration. Once-->
-<!--                        you've made the room you will be joined into it automatically, and from there can use generated invite codes to allow your other users-->
-<!--                        to join the room. Simply input the room generated invite code into the 'Join Room' field on this page, and you will be connected to that-->
-<!--                        sharing space.</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="slingContentDiv" style=" background-color: #38474F; width: 98%; height: 16%; margin-left: 1%; margin-right: 1%; margin-top: 1%;">-->
-<!--                <button class="sling-btn-main"-->
-<!--                  style=" width: 35%; margin: 2%; margin-left: 10%;  display: inline-block;  font-size: 1.3vw; overflow: hidden;"-->
-<!--                  onclick="Modal.create('Create Room Modal', 'darken', null)">-->
-<!--                  Create Room-->
-<!--                </button>-->
-<!--                <form class="" style="width: 35%; margin: 2%; margin-left: 8%; margin-top: 4%; display: inline-block"-->
-<!--                      onsubmit="joinroom(event, this);">-->
-<!--                    <input name="room" class="sling-mp-form" type="text" size="8" style="width: 100%; padding-left: 20%;  font-size: 1.3vw; overflow: hidden;"-->
-<!--                           value="Join Room" placeholder="Room Code" onfocus="toggleform(this)" onblur="toggleform(this)">-->
-<!--                </form>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="slingContentDiv" style=" background-color: #333333; border 6px solid black; height: 76%;    box-shadow: 10px 10px 5px black;">-->
-<!--            <div class="slingContentDiv" style="background-color: white; width: 98%; height: 10%; margin-left: 1%; margin-right: 1%; margin-top: 4%;">-->
-<!--                <span style="margin-left: 16%; color:#38474F; font-family:'Century Gothic'; font-size: 1.5vw; overflow: hidden;">[ Recent Updates ]</span>-->
-<!--            </div>-->
-<!--            <div class="slingContentDiv" style="background-color: transparent; width: 98%; height: 80%; margin-left: 1%; margin-right: 1%;">-->
-<!--                <script type="text/javascript" src="https://feed.mikle.com/js/fw-loader.js" data-fw-param="12535/"></script> <!-- end feedwind code -->-->
-<!--            </div>-->
-<!--        </div>-->
-    </div>
-
-</div>
+<body>
+<div class="slingBackground"></div>
 <div class="slingBackground slingBackgroundUpper"></div>
-<div class="slingContentDiv slingContentDivTitle">
-    <img  src="SlingLogo.png" alt="Sling S">
+<div class="container container-main-page">
+    <div class="sling-title-container">
+        <img class="sling-title" src="SlingLogo.png" title="Sling"/>
+        <div class="is-flex">
+            <div class="motto-div" style="border-right: 2px solid lightgray">
+                SHARE
+            </div>
+            <div class="motto-div">
+                STREAM
+            </div>
+            <div class="motto-div" style="border-left: 2px solid lightgray">
+                CONNECT
+            </div>
+        </div>
+    </div>
 </div>
-<div class="slingContentDiv slingContentDivMotto">
-    <div class="slingContentDivMottoInner">
-        <div class="slingContentDivCenterText">SHARE</div>
+
+<!--<div class="slingBackground slingBackgroundUpper">
+    <div class="slingContentDiv slingContentDivTitle">
+        <img style="height: 100%; width: 100%; object-fit: contain" src="SlingLogo.png" alt="Sling S">
     </div>
-    <div class="slingContentDivMottoInner" style="border-right: solid 3px cadetblue;border-left: solid 3px cadetblue;">
-        <div class="slingContentDivCenterText">STREAM</div>
+    <div class="slingContentDiv slingContentDivMotto">
+        <div class="slingContentDivMottoInner">
+            <div class="slingContentDivCenterText">SHARE</div>
+        </div>
+        <div class="slingContentDivMottoInner" style="border-right: solid 3px cadetblue;border-left: solid 3px cadetblue; margin-left: 20vw">
+            <div class="slingContentDivCenterText">STREAM</div>
+        </div>
+        <div class="slingContentDivMottoInner" style=" margin-left: 40vw">
+            <div class="slingContentDivCenterText">CONNECT</div>
+        </div>
     </div>
-    <div class="slingContentDivMottoInner">
-        <div class="slingContentDivCenterText">CONNECT</div>
-    </div>
-</div>
+</div>-->
+
+
 <div class="slingContentDiv slingContentDivButtons">
     <button class="slingButton slingButtonCR"
             onclick="Modal.create('Create Room Modal', 'darken', null)">
@@ -101,10 +73,10 @@ require_once "classes/Account.php";
 </div>
 
 
-<nav class="navbar " style="z-index: 999999">
+<!--<nav class="navbar " style="z-index: 999999">
     <div class="container-fluid">
         <div class="navbar-header">
-            <!--        Needs hamburger icon-->
+
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
@@ -122,30 +94,30 @@ require_once "classes/Account.php";
             </ul>
         </div>
     </div>
-</nav>
+</nav>-->
 
-<!--<div class="container-fluid"-->
-<!--               style="text-align: center; position: fixed; bottom: 0; width: 100%; background: #38474F;">-->
-<!--        <div style="margin: 0 auto; height: 20%;">-->
-<!--<!--            <button class="btn-main"-->-->
-<!--<!--                    style=" width: 200px; margin: 15px; margin-left: 30px; margin-right: 30px; display: inline-block"-->-->
-<!--<!--                    onclick="Modal.create('Create Room Modal', 'darken', null)">-->-->
-<!--<!--                Create Room-->-->
-<!--<!--            </button>-->-->
-<!--<!---->-->
-<!--<!--            <form class="" style="margin: 15px; margin-left: 30px; margin-right: 30px; display: inline-block"-->-->
-<!--<!--                  onsubmit="joinroom(event, this);">-->-->
-<!--<!--                <input name="room" class="mp-form" type="text" size="8" style="width: 200px; padding-left: 20px;"-->-->
-<!--<!--                       value="Join Room" placeholder="Room Code" onfocus="toggleform(this)" onblur="toggleform(this)">-->-->
-<!--<!--            </form>-->-->
-<!--        </div>-->
-<!--</div>-->
+<!--<div class="container-fluid"
+               style="text-align: center; position: fixed; bottom: 0; width: 100%; background: #38474F;">
+        <div style="margin: 0 auto; height: 20%;">
+            <button class="btn-main"
+                    style=" width: 200px; margin: 15px; margin-left: 30px; margin-right: 30px; display: inline-block"
+                    onclick="Modal.create('Create Room Modal', 'darken', null)">
+                Create Room
+            </button>
+
+            <form class="" style="margin: 15px; margin-left: 30px; margin-right: 30px; display: inline-block"
+                  onsubmit="joinroom(event, this);">
+                <input name="room" class="mp-form" type="text" size="8" style="width: 200px; padding-left: 20px;"
+                       value="Join Room" placeholder="Room Code" onfocus="toggleform(this)" onblur="toggleform(this)">
+            </form>
+        </div>
+</div>-->
 
 </body>
 <script type='text/javascript' src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
 <script type='text/javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script type='text/javascript' src="/assets/js/sling.js"></script>
-<iframe src="https://feed.mikle.com/widget/v2/12535/"></iframe>
+<!--<iframe src="https://feed.mikle.com/widget/v2/12535/"></iframe>-->
 <script>
 
     isLoggedIn();
